e400aeabe8884d33e0ba10f08e3432abf0199822
Mitchell Budge <mitchellbudge@gmail.com>
2019-12-09 19:21:37 -0700

    Add helper functions and main storyboard layout

--- Calculator/Calculator/View Controllers/CalculatorViewController.swift
+++ Calculator/Calculator/View Controllers/CalculatorViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    // MARK: - Properties and outlets
+    
+    var calculator: Calculator?
+    
+    // MARK: - View lifecycle methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        calculator = Calculator()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions and methods
+    
+    
+    
+    func clear() {
+        calculator = nil
+        calculator = Calculator()
     }
-    */
+
+    
 
 }
